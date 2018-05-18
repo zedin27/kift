604043b28c9f9ba72383525189784d1908714999
zedin27 <ztisneshamzeh@csumb.edu>
2018-05-18 01:09:34 -0700

    Add function to check if record file exists

--- js/record.js
+++ js/record.js
@@ -1,11 +1,32 @@
 const fs = require('fs');
 const record = require('node-record-lpcm16');
 
-var filename = "record.wav";
+// Used to name the recording file.
+var fileName = "record_";
+var fileNumber = 0;
+var fileExtension = ".wav"
 
-function startRecord() {
-	var file = fs.createWriteStream(filename, { encoding: 'binary' });
-	record.start({ sampleRate: 16000, verbose: true }).pipe(file);
+// Check if the filename exists.
+function newFile() {
+	while (1) {
+		// Join the strings to make the filename.
+		var file = fileName + fileNumber + fileExtension;
+
+		// Maybe we can refactor this part... guys, help?
+		if (fs.existsSync(file))
+			fileNumber++;
+		else
+			break;
+	}
+	return (file);
 }
 
-// startRecord();
+// Start recording.
+function startRecord() {
+	var file = newFile();
+	var fileRecord = fs.createWriteStream(file, { encoding: 'binary' });
+	record.start({ sampleRate: 16000, verbose: true }).pipe(fileRecord);
+	return ;
+}
+
+startRecord();
